df30e8b8c60c9651b4f40296f5bf402a4cb31839
galor <galor546@gmail.com>
2022-11-26 19:47:48 +0200

    esLint added and reformat all project

--- LiveChat/server.js
+++ LiveChat/server.js
@@ -9,24 +9,22 @@ const PORT = process.env.PORT || 3000;
 const admin = 'Chat Admin';
 
 // Create server
-const server = http.createServer((req,res) => {
-   let pathname = url.parse(req.url).pathname;
-   let ext = path.extname(pathname);
-   if (ext){
-      if (ext === '.css'){
-         res.writeHead(200, {'Content-Type': 'text/css'});
-      }
-      else if(ext === '.js'){
-         res.writeHead(200, {'Content-Type': 'text/javascript'});
-      }
-      let file = fs.createReadStream(__dirname + pathname);
-      file.pipe(res);
-   }
-   else{
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      let file = fs.createReadStream('./LiveChat/Frontend/index.html')
-      file.pipe(res);
-   }
+const server = http.createServer((req, res) => {
+    let pathname = url.parse(req.url).pathname;
+    let ext = path.extname(pathname);
+    if (ext) {
+        if (ext === '.css') {
+            res.writeHead(200, {'Content-Type': 'text/css'});
+        } else if (ext === '.js') {
+            res.writeHead(200, {'Content-Type': 'text/javascript'});
+        }
+        let file = fs.createReadStream(__dirname + pathname);
+        file.pipe(res);
+    } else {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        let file = fs.createReadStream('./LiveChat/Frontend/index.html')
+        file.pipe(res);
+    }
 });
 
 // create socket.io to server
@@ -34,29 +32,29 @@ const io = socketio(server);
 
 // Runs when client connects to our sever
 io.on('connection', socket => {
-   socket.on("joinChat", ({username}) => {
+    socket.on("joinChat", ({username}) => {
 
-      socket.emit('message', formatMessage(admin, `Hey ${username}, welcome to support chat`));
+        socket.emit('message', formatMessage(admin, `Hey ${username}, welcome to support chat`));
 
-      socket.on('typing', (data) => {
-         socket.broadcast.emit('display', data)
-      })
+        socket.on('typing', (data) => {
+            socket.broadcast.emit('display', data)
+        })
 
-      socket.on('chatMessage', msg => {
-         io.emit('message', formatMessage(username, msg));
-      });
+        socket.on('chatMessage', msg => {
+            io.emit('message', formatMessage(username, msg));
+        });
 
-      socket.on('close', msg => {
-         socket.broadcast.emit('message', formatMessage(admin, `${msg.user} ${msg.text}`));
-      });
-   });
+        socket.on('close', msg => {
+            socket.broadcast.emit('message', formatMessage(admin, `${msg.user} ${msg.text}`));
+        });
+    });
 });
 
 const turnOnServerChat = () => {
 
 
-   //init listener
-   server.listen(PORT, () => console.log(`server running on ${PORT}`));
+    //init listener
+    server.listen(PORT, () => console.log(`server running on ${PORT}`));
 }
 
 
