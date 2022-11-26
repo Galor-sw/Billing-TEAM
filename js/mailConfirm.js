15623224163c86d01741163bce57783d2c7f96f6
itay45977 <itay45977@gmail.com>
2022-11-26 12:59:45 +0200

    CSS changes

--- js/mailConfirm.js
+++ js/mailConfirm.js
@@ -8,17 +8,14 @@ $('input[name="submit"]').click( function(e) {
         $.post('http://localhost:8080/', mail)
             .done(function(msg)
                 {
-                    console.log("successes send to server");
-                    console.log(msg);
                     if(msg == "The email exist")
                     {
                         window.location.replace("http://localhost:8080/loginAndForm/form.html?mail="+mail);
                     }
                     else
                     {
-                        $('article').text(msg);
-                        $('article').css({'color': 'red'});
-
+                        failed=document.getElementById("failmsg");
+                        failed.innerHTML = msg;
                     }
 
                 })
@@ -27,6 +24,10 @@ $('input[name="submit"]').click( function(e) {
                 console.log("failed send to server"+ error);
 
             });
+    }
+    else
+    {
+        $('article').text("Please insert an email address");
     }
 
 });
