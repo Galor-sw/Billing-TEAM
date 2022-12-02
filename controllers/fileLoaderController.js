89b622d35364b98bfe99b0e473cbcf80cdb32d5c
Alona <alonarozner4@gmail.com>
2022-12-02 13:09:01 +0200

    try3 env

--- controllers/fileLoaderController.js
+++ controllers/fileLoaderController.js
@@ -13,8 +13,6 @@ process.env.STATUS === 'production'
 module.exports = {
 
     loadLoginFile: (req, res) => {
-        // console.log(path.join(__dirname, pathValidator , '/loginAndForm/log_in_form.html'));
-        // console.log((path.join(__dirname, '../loginAndForm/log_in_form.html')));
         process.env.STATUS === 'production'
             ? res.sendFile('../loginAndForm/log_in_form.html')
             : res.sendFile(path.join(__dirname, '../loginAndForm/log_in_form.html'));
