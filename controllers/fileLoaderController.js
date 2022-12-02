1b79aa29d33d53a931f32b276dd54bdeeda8a467
Alona <alonarozner4@gmail.com>
2022-12-02 16:55:55 +0200

    try fix url rander try7

--- controllers/fileLoaderController.js
+++ controllers/fileLoaderController.js
@@ -15,20 +15,20 @@ module.exports = {
     loadLoginFile: (req, res) => {
         console.log("heyyyyy");
         process.env.STATUS === 'production'
-            ? res.sendFile('log_in_form.html', { root: pathValidator})
+            ? res.sendFile('/loginAndForm/log_in_form.html', { root: pathValidator})
             : res.sendFile(path.join(__dirname, '../loginAndForm/log_in_form.html'));
         // res.sendFile(path.join(__dirname, '../loginAndForm/log_in_form.html'));
 
     },
     loadFormFile: (req, res) => {
         process.env.STATUS === 'production'
-            ? res.sendFile('form.html', { root: pathValidator})
+            ? res.sendFile('/loginAndForm/form.html', { root: pathValidator})
             : res.sendFile(path.join(__dirname, '../loginAndForm/form.html'));
         // res.sendFile(path.join(__dirname, '../loginAndForm/form.html'));
     },
     loadMessageFile: (req, res) => {
         process.env.STATUS === 'production'
-            ? res.sendFile('message.html', { root: pathValidator})
+            ? res.sendFile('/loginAndForm/message.html', { root: pathValidator})
             : res.sendFile(path.join(__dirname, '../loginAndForm/message.html'));
         // res.sendFile(path.join(__dirname, '../loginAndForm/message.html'));
     }
