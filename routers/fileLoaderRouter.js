0373386f0d6bbda266ecf8f7bb6b1084532afe59
Alona <alonarozner4@gmail.com>
2022-12-02 19:30:34 +0200

    try fix url rander try21

--- routers/fileLoaderRouter.js
+++ routers/fileLoaderRouter.js
@@ -3,6 +3,9 @@ const fileLoaderController= require("../controllers/fileLoaderController");
 const fileLoaderRouter = express.Router();
 
 fileLoaderRouter.get('/',fileLoaderController.loadLoginFile);
+
 fileLoaderRouter.get('/loginAndForm/', fileLoaderController.loadFormFile);
+
 fileLoaderRouter.get('/loginAndForm/',fileLoaderController.loadMessageFile);
+
 module.exports = fileLoaderRouter;
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const userRouter = express.Router();
 const userController= require("../controllers/userController");
-// const userRouter = new express();
 
 userRouter.post('/', userController.emailCheck);
 
