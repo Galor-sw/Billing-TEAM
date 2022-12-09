036376e5bb275218a024e2ef7576716f7a3b8e0e
itay45977 <itay45977@gmail.com>
2022-12-09 16:54:28 +0200

    Made a switch - the email validation will run through IAM's API instead of our function. Added a backup function in case IAM's server is down.

--- js/mailConfirm.js
+++ js/mailConfirm.js
@@ -1,26 +1,48 @@
 // Input email confirmation
+const IAMbackupbyJson = () => {
+    let mail = $('input[name="mail"]').val();
+    let jsonMail = {"mail": mail};
+    if (mail) {
+        $.post(`${window.location.origin}/emailCheck`, jsonMail)
+            .done(function (msg) {
+                if (msg == "The email exists") {
+                    window.location.replace(`${window.location.origin}/loginAndForm/form.html?mail=${mail}`);
+                } else {
+                    document.getElementById("failmsg").innerHTML = msg;
+                }
+            })
+            .fail((xhr, status, error) => {
+                console.log("failed send to server" + error);
+
+            })
+        // Empty input
+    } else {
+        document.getElementById("failmsg").innerHTML = "Please insert an email address";
+    }
+}
 
 $("document").ready(() => {
     $('input[name="submit"]').click((e) => {
         e.preventDefault();
         let mail = $('input[name="mail"]').val();
-        let jsonMail = {"mail": mail};
+        // Adding here the API of IAM - we'll validate through their server
         if (mail) {
-            $.post(`${window.location.origin}/emailCheck`, jsonMail)
-                .done(function (msg) {
-                    if (msg == "The email exists") {
+            $.get(`http://iam-team.onrender.com/api/login/${mail}`)
+                .done(msg => {
+                    if (msg == "The user exists")
+                    {
                         window.location.replace(`${window.location.origin}/loginAndForm/form.html?mail=${mail}`);
-                    } else {
-                        document.getElementById("failmsg").innerHTML = msg;
+                    }
+                    else {
+                        document.getElementById("failmsg").innerHTML = "The email does not exist, try again";
                     }
                 })
                 .fail((xhr, status, error) => {
-                    console.log("failed send to server" + error);
-
+                    IAMbackupbyJson();
                 })
-            // Empty input
-        } else {
-            document.getElementById("failmsg").innerHTML = "Please insert an email address";
         }
+
+
+
     });
 });
